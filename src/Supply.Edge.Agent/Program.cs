6f363659e627136afd7e02814832fe2b105d1fad
José Rogério de Almeida Júnior <almeidajr.vr@gmail.com>
2026-02-09 21:07:47 -0300

    feat: integrate Serilog on Edge components

--- src/Supply.Edge.Agent/Program.cs
+++ src/Supply.Edge.Agent/Program.cs
@@ -1,6 +1,14 @@
+using Serilog;
 using Supply.Edge.Agent;
 
 var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddSerilog(
+    (services, loggerConfiguration) =>
+        loggerConfiguration
+            .ReadFrom.Configuration(builder.Configuration)
+            .ReadFrom.Services(services)
+            .Enrich.FromLogContext()
+);
 builder.Services.AddHostedService<Worker>();
 
 var host = builder.Build();
